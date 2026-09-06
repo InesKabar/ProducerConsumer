6a55e37e2b2f0b11db6a86f136aef7eeb6659027
Ines Kabar <86208137+InesKabar@users.noreply.github.com>
2026-09-06 17:30:29 -0400

    Update consommateur.py

--- consommateur.py
+++ consommateur.py
@@ -1,5 +1,6 @@
 import threading
 
+
 class Consommateur(threading.Thread):
     def __init__(self, bte, lock):
         threading.Thread.__init__(self)
@@ -8,20 +9,20 @@ class Consommateur(threading.Thread):
         self.nb_msg_lus = 0
         self.lock = lock
 
-
     def log(self, msg):
         print(f'Admin : {msg}\n')
 
-
-
     def run(self):
         self.log('initialisé')
+
         while self.actif:
             self.lock.acquire()
+
             if self.boite.est_pleine():
                 val = self.boite.retirer_val()
-                self.log(f' j\'ai retire {val}')
+                self.log(f" j'ai retire {val}")
                 self.nb_msg_lus += 1
+
                 if self.nb_msg_lus == 3:
                     self.actif = False
 
