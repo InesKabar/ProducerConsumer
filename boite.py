292c6755b631ecad440f6528b052eb5a4554985f
Ines Kabar <86208137+InesKabar@users.noreply.github.com>
2026-09-06 17:29:20 -0400

    Update boite.py

--- boite.py
+++ boite.py
@@ -1,26 +1,23 @@
 import threading
+
+
 class Boite:
     def __init__(self):
         self.pleine = False
         self.valeur = None
 
-
     def est_vide(self):
-        #with self.lock:
-            return not self.pleine
+        return not self.pleine
 
     def est_pleine(self):
-        #with self.lock:
-            return self.pleine
+        return self.pleine
 
     def ajouter_val(self, val):
-        #with self.lock:
-            if not self.est_pleine():
-                self.valeur = val
-                self.pleine = True
+        if not self.est_pleine():
+            self.valeur = val
+            self.pleine = True
 
     def retirer_val(self):
-        #with self.lock:
-            if self.est_pleine():
-                self.pleine = False
-                return self.valeur
+        if self.est_pleine():
+            self.pleine = False
+            return self.valeur
